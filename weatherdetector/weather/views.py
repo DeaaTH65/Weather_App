a406615508db9e931cff8f74981f00884843224d
Deaath65 <deaath65@gmail.com>
2023-06-17 10:35:58 +0545

    views completed/api key

--- weatherdetector/weather/views.py
+++ weatherdetector/weather/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        city = request.POST['city']
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=cb771e45ac79a4e8e2205c0ce66ff633').read()   # ApiKey 11e89f3af429371b79751d6b7fc03b4a
+        json_data = json.loads(res)
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' +
+            str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp'])+'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+
+    else:
+        city = ''
+        data = {}
+    return render(request, 'index.html', {'city': city, 'data': data})
